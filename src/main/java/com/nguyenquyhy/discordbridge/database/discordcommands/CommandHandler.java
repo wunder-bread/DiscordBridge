063ac85a024b8992f07e04d3cdfbfd20f9918a77
axle2005 <arnoldryan2005@live.ca>
2017-06-29 13:31:36 -0300

    Issue #63 Feature 1 Working - Not Customizable Yet.

---
+++ src/main/java/com/nguyenquyhy/discordbridge/database/discordcommands/CommandHandler.java
@@ -0,0 +1,98 @@
+package com.nguyenquyhy.discordbridge.database.discordcommands;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import com.nguyenquyhy.discordbridge.DiscordBridge;
+import com.nguyenquyhy.discordbridge.models.ChannelConfig;
+import com.nguyenquyhy.discordbridge.models.GlobalConfig;
+import com.nguyenquyhy.discordbridge.utils.ChannelUtil;
+import com.nguyenquyhy.discordbridge.utils.ErrorMessages;
+import com.nguyenquyhy.discordbridge.utils.TextUtil;
+
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.entities.Channel;
+import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.entities.message.Message;
+
+public class CommandHandler {
+
+    private static String id = "discordbridge";
+
+    public static void handle(Optional<Player> player, String substring) {
+	DiscordBridge mod = DiscordBridge.getInstance();
+	mod.getLogger().info("Working");
+	queueCommand(player, substring.toLowerCase().split(" "));
+
+    }
+
+    public static void handle(Message message) {
+	DiscordBridge mod = DiscordBridge.getInstance();
+	String content = message.getContent().substring(1);
+	queueCommand(message.getAuthor(), message.getChannelReceiver(),mod, content.toLowerCase().split(" "));
+    }
+
+    private static void queueCommand(Optional<Player> player, String[] strings) {
+	if (!(strings.length > 1)) {
+	    sendMessageToSource(player, 0);
+	    return;
+	}
+
+	if (strings[0].equals("ban") && permCheck(player, id + "ban")) {
+
+	}
+
+    }
+    private static void queueCommand(User user,Channel channel,DiscordBridge mod, String[] strings) {
+	if(strings[0].equals("tps")) TpsCommand.run(mod);
+	else if(strings[0].equals("players")) PlayerCommand.run(mod, channel);
+
+    }
+
+    private static boolean permCheck(Optional<Player> player, String permission) {
+	if (!player.isPresent())
+	    return true;
+	else if (player.get().hasPermission(permission))
+	    return true;
+	else
+	    return false;
+    }
+
+    private static void sendMessageToSource(Optional<Player> player, int code) {
+	CommandSource src;
+	if (player.isPresent())
+	    src = Sponge.getServer().getPlayer(player.get().getUniqueId()).get().getCommandSource().get();
+	else
+	    src = Sponge.getServer().getConsole();
+
+	if (code == 0) {
+	    src.sendMessage(Text.of(TextColors.RED, "Not enough arguments"));
+	}
+
+    }
+
+    private static void runCommand(String substring) {
+	DiscordBridge mod = DiscordBridge.getInstance();
+	GlobalConfig config = mod.getConfig();
+
+	DiscordAPI client = mod.getBotClient();
+
+	
+	for (ChannelConfig channelConfig : config.channels) {
+	    Channel channel = client.getChannelById(channelConfig.discordId);
+	    if (channel == null) {
+		ErrorMessages.CHANNEL_NOT_FOUND.log(channelConfig.discordId);
+		return;
+	    }
+	    ChannelUtil.banPlayer(channel, substring);
+	}
+
+    }
+}
