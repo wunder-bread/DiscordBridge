f79323ac5cb11013e6b8f5b009baea1953575f84
axle2005 <arnoldryan2005@live.ca>
2017-07-08 11:23:25 -0300

    Rollback version to allow pull request.

--- src/main/java/com/nguyenquyhy/discordbridge/DiscordBridge.java
+++ src/main/java/com/nguyenquyhy/discordbridge/DiscordBridge.java
@@ -32,7 +32,7 @@ import java.util.*;
 /**
  * Created by Hy on 1/4/2016.
  */
-@Plugin(id = "discordbridge", name = "Discord Bridge", version = "2.5.0",
+@Plugin(id = "discordbridge", name = "Discord Bridge", version = "2.3.0",
         description = "A Sponge plugin to connect your Minecraft server with Discord", authors = {"Hy", "Mohron, Axle2005"})
 public class DiscordBridge {
 
